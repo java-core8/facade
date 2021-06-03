a67d2847e3378946e94be9548e5b59c5e94a0b1e
TomuraCreator <vladislavutkin92@gmail.com>
2021-06-03 15:08:56 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static public void main(String ...args) {
+        BinOps binOps = new BinOps();
+
+        System.out.println(binOps.sum("01110111", "01011101"));
+        System.out.println(binOps.mult("01110111", "01011101"));
+    }
+}
